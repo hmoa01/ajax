be04afe4e9323d64cf7b2a9514681ba711308462
hmoa01 <hmoa01@gmail.com>
2023-05-09 14:44:05 +0200

    add more navbar brands and data

--- main.js
+++ main.js
@@ -1,15 +1,75 @@
+//SELECTORS
 let tHead = document.querySelector("thead");
 let tBody = document.querySelector("tbody");
 
+//BUTTONS
+let planetsBtn = document.querySelector("#planetsBtn");
+let teamsBtn = document.querySelector("#teamsBtn");
+let dinosaursBtn = document.querySelector("#dinosaursBtn");
+let bestNovelsBtn = document.querySelector("#bestNovelsBtn");
+let continentsBtn = document.querySelector("#continentsBtn");
+let birdsBtn = document.querySelector("#birdsBtn");
+
+//LISTENERS
+planetsBtn.addEventListener("click", displayPlanets);
+teamsBtn.addEventListener("click", displayAmericanTeams);
+dinosaursBtn.addEventListener("click", displayDinosaurs);
+bestNovelsBtn.addEventListener("click", displayBestNovels);
+continentsBtn.addEventListener("click", displayContinents);
+birdsBtn.addEventListener("click", displayBirds);
+
+//GET DATA
 let data = api.getData(
   "https://mysafeinfo.com/api/data?list=teamlistus&format=json&case=default"
 );
+data.then(displayTable);
 
-data.then(displayCards);
+//FUNCTIONS
+displayTable(data);
 
-displayCards(data);
+function displayBirds() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=birdsus&format=json&case=default"
+  );
+  data.then(displayTable);
+}
 
-function displayCards(data) {
+function displayContinents() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=continents&format=json&case=default"
+  );
+  data.then(displayTable);
+}
+
+https: function displayBestNovels() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=bestnovels&format=json&case=default"
+  );
+  data.then(displayTable);
+}
+
+function displayDinosaurs() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=dinosaurs&format=json&case=default"
+  );
+  data.then(displayTable);
+}
+
+function displayAmericanTeams() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=teamlistus&format=json&case=default"
+  );
+  data.then(displayTable);
+}
+
+function displayPlanets() {
+  data = api.getData(
+    "https://mysafeinfo.com/api/data?list=planets&format=json&case=default"
+  );
+  data.then(displayTable);
+}
+
+function displayTable(data) {
   console.log(data);
   let html = `<tr>`;
   let first = data[0];
@@ -28,4 +88,5 @@ function displayCards(data) {
     html += `</tr>`;
     tBody.innerHTML = html;
   });
+  return data;
 }
